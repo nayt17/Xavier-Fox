8a6e5c9ba1612c1a9509504aef6590d510072635
nayt17 <42542749+nayt17@users.noreply.github.com>
2018-09-22 11:04:14 +0700

    Add files via upload

---
+++ Week05/exercise-08.js
@@ -0,0 +1,13 @@
+function cariPelaku(str) {
+    // you can only write your code here!
+    var panjang = str.match(/abc/g)
+    return panjang.length
+    
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
